98081e2f42f81109e73ca0e305d6dcb96cffac10
Hieu Pham <phamduchieu0310@gmail.com>
2025-10-15 09:54:45 +0700

    basic sub string

---
+++ Basic Ruby Projects/sub_strings.rb
@@ -0,0 +1,19 @@
+test_dictionary_1 = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+test_str_1 = "Howdy partner, sit down! How's it going?"
+
+def substring(str, dict)
+  result = Hash.new(0)
+  str_array = str.split(" ")
+  
+  str_array.each do |m_str|
+    dict.each do |dict_str|
+      if m_str.include? dict_str
+        result[dict_str] += 1
+      end
+    end
+  end
+
+  p result
+end 
+
+substring(test_str_1, test_dictionary_1)
